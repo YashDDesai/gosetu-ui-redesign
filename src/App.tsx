f54d00482f112a9792515b0048905b3b9f995e80
Yash <yash@weenggs.com>
2022-07-07 16:48:44 +0530

    dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
+import Groups from "./components/Groups";
+import Machines from "./components/Machines";
+import Profile from "./components/Profile";
+import Reports from "./components/Reports";
+import Settings from "./components/Settings";
 import SidebarWithHeader from "./components/SidebarWithHeader";
 
 export default function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-      </Routes>
-      <SidebarWithHeader>{}</SidebarWithHeader>
+      <SidebarWithHeader>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/machines" element={<Machines />} />
+          <Route path="/groups" element={<Groups />} />
+          <Route path="/reports" element={<Reports />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </SidebarWithHeader>
     </>
   );
 }
--- src/components/SidebarWithHeader.tsx
+++ src/components/SidebarWithHeader.tsx
@@ -1,6 +1,5 @@
 import {
   Avatar,
-  Image,
   Box,
   BoxProps,
   CloseButton,
@@ -8,9 +7,11 @@ import {
   DrawerContent,
   Flex,
   FlexProps,
+  Hide,
   HStack,
   Icon,
   IconButton,
+  Image,
   Link,
   Menu,
   MenuButton,
@@ -21,33 +22,30 @@ import {
   useColorModeValue,
   useDisclosure,
   VStack,
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+  StatGroup,
 } from "@chakra-ui/react";
 import { ReactNode } from "react";
-import {
-  FiBell,
-  FiChevronDown,
-  FiFile,
-  FiList,
-  FiMenu,
-  FiSettings,
-} from "react-icons/fi";
-import { GoDashboard } from "react-icons/go";
+import { FiChevronDown, FiMenu } from "react-icons/fi";
 
 import { IconType } from "react-icons";
-import { GiSewingMachine } from "react-icons/gi";
-import Logo from "../assets/Gosetu_logo.png";
 import { Link as ReactRouterLink } from "react-router-dom";
+import Logo from "../assets/Gosetu_logo.png";
 
 interface LinkItemProps {
   name: string;
-  icon: IconType;
+  to: string;
 }
 const LinkItems: Array<LinkItemProps> = [
-  { name: "Dashboard", icon: GoDashboard },
-  { name: "Machines", icon: GiSewingMachine },
-  { name: "Groups", icon: FiList },
-  { name: "Reports", icon: FiFile },
-  { name: "Settings", icon: FiSettings },
+  { name: "Dashboard", to: "/" },
+  { name: "Machines", to: "/machines" },
+  { name: "Groups", to: "/groups" },
+  { name: "Reports", to: "/reports" },
+  { name: "Settings", to: "/settings" },
 ];
 
 export default function SidebarWithHeader({
@@ -56,28 +54,33 @@ export default function SidebarWithHeader({
   children: ReactNode;
 }) {
   const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
-      <SidebarContent
+      {/* <SidebarContent
         onClose={() => onClose}
         display={{ base: "none", md: "block" }}
-      />
+      /> */}
+
       <Drawer
         autoFocus={false}
         isOpen={isOpen}
         placement="left"
         onClose={onClose}
-        returnFocusOnClose={false}
+        returnFocusOnClose={true}
         onOverlayClick={onClose}
-        size="full"
+        closeOnOverlayClick={true}
+        closeOnEsc={true}
+        // size="full"
       >
         <DrawerContent>
           <SidebarContent onClose={onClose} />
         </DrawerContent>
       </Drawer>
+
       {/* mobilenav */}
       <MobileNav onOpen={onOpen} />
-      <Box ml={{ base: 0, md: 60 }} p="4">
+      <Box ml={{ base: 0, md: 0 }} p="4">
         {children}
       </Box>
     </Box>
@@ -95,13 +98,13 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
       bg={useColorModeValue("white", "gray.900")}
       borderRight="1px"
       borderRightColor={useColorModeValue("gray.200", "gray.700")}
-      w={{ base: "full", md: 60 }}
+      w="full"
       pos="fixed"
       h="full"
       {...rest}
     >
       <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
-        <Box bg="black" padding={3} borderRadius={6}>
+        <Box bg="black" padding={3} borderRadius={6} onClick={onClose}>
           <Link as={ReactRouterLink} to="/">
             <Image src={Logo} w={24} alt="logo" />
           </Link>
@@ -112,9 +115,11 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
         <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
       </Flex>
       {LinkItems.map((link) => (
-        <NavItem key={link.name} icon={link.icon}>
-          {link.name}
-        </NavItem>
+        <ReactRouterLink to={link.to}>
+          <NavItem key={link.name} icon={() => <></>} onClick={onClose}>
+            {link.name}
+          </NavItem>
+        </ReactRouterLink>
       ))}
     </Box>
   );
@@ -165,84 +170,127 @@ interface MobileProps extends FlexProps {
 }
 const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
   return (
-    <Flex
-      ml={{ base: 0, md: 60 }}
-      px={{ base: 4, md: 4 }}
-      height="20"
-      alignItems="center"
-      bg={useColorModeValue("white", "gray.900")}
-      borderBottomWidth="1px"
-      borderBottomColor={useColorModeValue("gray.200", "gray.700")}
-      justifyContent={{ base: "space-between", md: "flex-end" }}
-      {...rest}
-    >
-      <IconButton
-        display={{ base: "flex", md: "none" }}
-        onClick={onOpen}
-        variant="outline"
-        aria-label="open menu"
-        icon={<FiMenu />}
-      />
-
-      <Text
-        display={{ base: "flex", md: "none" }}
-        fontSize="2xl"
-        fontFamily="monospace"
-        fontWeight="bold"
+    <Box>
+      <Flex
+        // ml={{ base: 0, md: 60 }}
+        px={{ base: 4, md: 4 }}
+        height="20"
+        alignItems="center"
+        bg={useColorModeValue("white", "gray.900")}
+        borderBottomWidth="1px"
+        borderBottomColor={useColorModeValue("gray.200", "gray.700")}
+        justifyContent={{ base: "space-between" }}
+        {...rest}
       >
-        Logo
-      </Text>
-
-      <HStack spacing={{ base: "0", md: "6" }}>
         <IconButton
-          size="lg"
-          variant="ghost"
+          display={{ base: "flex" }}
+          onClick={onOpen}
+          variant="outline"
           aria-label="open menu"
-          icon={<FiBell />}
+          icon={<FiMenu />}
         />
-        <Flex alignItems={"center"}>
-          <Menu>
-            <MenuButton
-              py={2}
-              transition="all 0.3s"
-              _focus={{ boxShadow: "none" }}
-            >
-              <HStack>
-                <Avatar
-                  size={"sm"}
-                  src={
-                    "https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
-                  }
-                />
-                <VStack
-                  display={{ base: "none", md: "flex" }}
-                  alignItems="flex-start"
-                  spacing="1px"
-                  ml="2"
-                >
-                  <Text fontSize="sm">Justina Clark</Text>
-                  <Text fontSize="xs" color="gray.600">
-                    Admin
-                  </Text>
-                </VStack>
-                <Box display={{ base: "none", md: "flex" }}>
-                  <FiChevronDown />
-                </Box>
-              </HStack>
-            </MenuButton>
-            <MenuList
-              bg={useColorModeValue("white", "gray.900")}
-              borderColor={useColorModeValue("gray.200", "gray.700")}
-            >
-              <MenuItem>Profile</MenuItem>
-              <MenuItem>Settings</MenuItem>
-              <MenuItem>Billing</MenuItem>
-              <MenuDivider />
-              <MenuItem>Sign out</MenuItem>
-            </MenuList>
-          </Menu>
-        </Flex>
-      </HStack>
-    </Flex>
+        <HStack py={1}>
+          <Stat bg="gray.400" p={2} w={{ base: 20, md: 120 }} borderRadius={4}>
+            <StatLabel textAlign={"center"} fontSize={{ base: 8, md: 12 }}>
+              No Power
+            </StatLabel>
+            <StatNumber textAlign={"center"} fontSize={{ base: 9, md: 14 }}>
+              13
+            </StatNumber>
+            {/* <StatHelpText>Feb 12 - Feb 28</StatHelpText> */}
+          </Stat>
+          <Stat bg="red.400" p={2} w={{ base: 20, md: 120 }} borderRadius={4}>
+            <StatLabel textAlign={"center"} fontSize={{ base: 8, md: 12 }}>
+              Filler Stop
+            </StatLabel>
+            <StatNumber textAlign={"center"} fontSize={{ base: 9, md: 14 }}>
+              2
+            </StatNumber>
+            {/* <StatHelpText>Feb 12 - Feb 28</StatHelpText> */}
+          </Stat>
+          <Stat bg="blue.400" p={2} w={{ base: 20, md: 120 }} borderRadius={4}>
+            <StatLabel textAlign={"center"} fontSize={{ base: 8, md: 12 }}>
+              Manual Stop
+            </StatLabel>
+            <StatNumber textAlign={"center"} fontSize={{ base: 9, md: 14 }}>
+              2
+            </StatNumber>
+            {/* <StatHelpText>Feb 12 - Feb 28</StatHelpText> */}
+          </Stat>
+          <Stat bg="green.400" p={2} w={{ base: 20, md: 120 }} borderRadius={4}>
+            <StatLabel textAlign={"center"} fontSize={{ base: 8, md: 12 }}>
+              Manual Stop
+            </StatLabel>
+            <StatNumber textAlign={"center"} fontSize={{ base: 9, md: 14 }}>
+              2
+            </StatNumber>
+            {/* <StatHelpText>Feb 12 - Feb 28</StatHelpText> */}
+          </Stat>
+          <Stat
+            bg="orange.400"
+            p={2}
+            w={{ base: 20, md: 120 }}
+            borderRadius={4}
+          >
+            <StatLabel textAlign={"center"} fontSize={{ base: 8, md: 12 }}>
+              Manual Stop
+            </StatLabel>
+            <StatNumber textAlign={"center"} fontSize={{ base: 9, md: 14 }}>
+              2
+            </StatNumber>
+            {/* <StatHelpText>Feb 12 - Feb 28</StatHelpText> */}
+          </Stat>
+        </HStack>
+        {/* <Hide above="sm">
+          <Box bg="black" padding={3} borderRadius={6}>
+            <Link as={ReactRouterLink} to="/">
+              <Image src={Logo} w={24} alt="logo" />
+            </Link>
+          </Box>
+        </Hide> */}
+
+        <HStack spacing={{ base: "0", md: "6" }}>
+          <Flex alignItems={"center"}>
+            <Menu>
+              <MenuButton
+                py={2}
+                transition="all 0.3s"
+                _focus={{ boxShadow: "none" }}
+              >
+                <HStack>
+                  <Avatar name="Yash Desai" size={"sm"} src="#" />
+                  <VStack
+                    display={{ base: "none", md: "flex" }}
+                    alignItems="flex-start"
+                    spacing="1px"
+                    ml="2"
+                  >
+                    <Text fontSize="sm">Yash Desai</Text>
+                  </VStack>
+                  <Box display={{ base: "none", md: "flex" }}>
+                    <FiChevronDown />
+                  </Box>
+                </HStack>
+              </MenuButton>
+              <MenuList
+                bg={useColorModeValue("white", "gray.900")}
+                borderColor={useColorModeValue("gray.200", "gray.700")}
+              >
+                <ReactRouterLink to="/profile">
+                  <MenuItem>Profile</MenuItem>
+                </ReactRouterLink>
+                <ReactRouterLink to="/settings">
+                  <MenuItem>Settings</MenuItem>
+                </ReactRouterLink>
+                <MenuDivider />
+                <MenuItem onClick={() => alert("logged out")}>
+                  Sign out
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+        </HStack>
+      </Flex>
+    </Box>
   );
 };
